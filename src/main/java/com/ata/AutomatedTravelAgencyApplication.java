4b56ed194d39549ce12cc5116e91e3284e0693d0
theanuragharsh <nrgharsh@gmail.com>
2023-08-05 12:52:53 +0530

    Initial commit

---
+++ src/main/java/com/ata/AutomatedTravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.ata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTravelAgencyApplication.class, args);
+	}
+
+}
